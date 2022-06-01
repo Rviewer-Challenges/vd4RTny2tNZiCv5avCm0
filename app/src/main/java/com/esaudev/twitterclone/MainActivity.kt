b99654ab066fe850e8dad63061d05b72f04beb29
Esau <yosoypawn@hotmail.com>
2022-06-01 15:50:08 -0500

    feat: bottom nav with fragments

--- app/src/main/java/com/esaudev/twitterclone/MainActivity.kt
+++ app/src/main/java/com/esaudev/twitterclone/MainActivity.kt
@@ -2,10 +2,22 @@ package com.esaudev.twitterclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.esaudev.twitterclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController = Navigation.findNavController(this, R.id.fcvMainContainer)
+        NavigationUI.setupWithNavController(binding.bnvMain, navController)
     }
 }
